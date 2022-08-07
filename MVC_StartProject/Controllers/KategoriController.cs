5f785d8b4ef2924ad3fa928cf44e5cb1f7d483aa
Onat _PC <onat.somer2018@gmail.com>
2022-08-07 17:44:09 +0300

    Add project files.

---
+++ MVC_StartProject/Controllers/KategoriController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StartProject.Models;
+
+namespace MVC_StartProject.Controllers
+{
+    public class KategoriController : Controller
+    {
+        KitaplikDBwebEntities kitaplikDB = new KitaplikDBwebEntities();
+        public ActionResult Index()
+        {
+            var kategoriList = kitaplikDB.KategoriTbl.ToList();
+            return View(kategoriList);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(KategoriTbl s)
+        {
+            kitaplikDB.KategoriTbl.Add(s);
+            kitaplikDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var categori = kitaplikDB.KategoriTbl.Find(id);
+            kitaplikDB.KategoriTbl.Remove(categori);
+            kitaplikDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var categoryGetir = kitaplikDB.KategoriTbl.Find(id);
+            return View("KategoriGetir", categoryGetir);
+        }
+
+        public ActionResult KategoriGuncelle(KategoriTbl u)
+        {
+            var categoryUpdate = kitaplikDB.KategoriTbl.Find(u.ID);
+            categoryUpdate.KategoriAd = u.KategoriAd;
+            kitaplikDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_StartProject/Controllers/KitapController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StartProject.Models;
+
+namespace MVC_StartProject.Controllers
+{
+    public class KitapController : Controller
+    {
+        KitaplikDBwebEntities db = new KitaplikDBwebEntities();
+        public ActionResult Index()
+        {
+            var kitaplar = db.KitapTbl.ToList();
+            return View(kitaplar);
+        }
+
+        [HttpGet]
+        public ActionResult KitapEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KitapEkle(KitapTbl t)
+        {
+            db.KitapTbl.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KitapSil(int id)
+        {
+            var kitap = db.KitapTbl.Find(id);
+            db.KitapTbl.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KitapGetir(int id)
+        {
+            var kitap = db.KitapTbl.Find(id);
+            return View("KitapGetir", kitap);
+        }
+
+        public ActionResult KitapGuncelle(KitapTbl kitapTbl)
+        {
+            var ktp = db.KitapTbl.Find(kitapTbl.KitapID);
+            ktp.Adi = kitapTbl.Adi;
+            ktp.Sayfano = kitapTbl.Sayfano;
+            ktp.Yazar = kitapTbl.Yazar;
+            ktp.Kategori = kitapTbl.Kategori;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
